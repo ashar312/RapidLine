b36e8c136fe4e8ed626558b3ac54aaa3b38fcc2d
Spirit0005 <m_hamzak1997@yahoo.com>
2020-02-05 16:52:02 +0500

    added register entries form

---
+++ app/src/main/java/com/project/rapidline/Register_Entries.java
@@ -0,0 +1,14 @@
+package com.project.rapidline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_Entries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register__entries);
+    }
+}
